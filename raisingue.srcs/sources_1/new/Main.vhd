b2106fbfa7ad0e9b32e2283211cac8ddf21bcb84
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-05-19 00:20:16 +0200

    style: reducing a bit the prescaler

--- raisingue.srcs/sources_1/new/Main.vhd
+++ raisingue.srcs/sources_1/new/Main.vhd
@@ -3,7 +3,7 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY Main IS
     GENERIC (
-        g_num_bits : INTEGER := 21
+        g_num_bits : INTEGER := 19 --21 is nice
     );
     PORT (
         PCLOCK : IN STD_LOGIC;
